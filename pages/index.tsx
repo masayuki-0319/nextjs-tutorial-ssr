4b3825dd881df072787bc45938457ea43021f5b4
将之大西 <omccello@gmail.com>
2021-10-27 22:00:03 +0900

    lec89

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,46 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 
-const Home: NextPage = () => {
+type Product = {
+  id: string;
+  title: string;
+  description: string;
+};
+
+type Props = {
+  products: Product[];
+};
+
+const Home: NextPage<Props> = (props) => {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => {
+        return <li key={product.id}>{product.title}</li>;
+      })}
     </ul>
   );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const products: Product[] = [
+    {
+      id: 'p1',
+      title: 'Product 1',
+      description: 'Description 1',
+    },
+    {
+      id: 'p2',
+      title: 'Product 2',
+      description: 'Description 2',
+    },
+  ];
+
+  return {
+    props: {
+      products: products,
+    },
+  };
 };
 
 export default Home;
